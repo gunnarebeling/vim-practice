5826d1c62e2764b5f9c0f1fa652494b90c463bfa
gunnarebeling <gunnarebeling@icloud.com>
2026-01-31 19:57:23 -0600

    first commit

---
+++ learning/ch2-test.js
@@ -0,0 +1,8 @@
+// Calculates sum of item prices
+function sumPrices(items) {
+    let total = 0;
+    for (let i = 0; i < items.length; i++) {
+        total += items[i].cost;
+    }
+    return total;
+}
